dbe0033df6fefc9aaffee0021df6fb8ba28f4bc6
Ian Butler <itb206@nyu.edu>
2022-07-12 21:41:40 -0400

    Move files around

--- gen/src/main.rs
+++ gen/src/main.rs
@@ -1,5 +1,15 @@
 use std::process::Command;
 
 fn main() {
-    
+    let output = Command::new("source").arg("./build_files/torb_env/bin/activate").output();
+
+    match output {
+        Ok(res) => {
+            dbg!(res);
+        }
+        Err(e) => {
+            dbg!(e);
+        }
+    }
+
 }
